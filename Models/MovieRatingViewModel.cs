3b519018e96d803214b3f158316d068e67a0167f
Alexey Khromets <alexchromets@gmail.com>
2017-08-14 14:51:17 +0300

    test commit

---
+++ Models/MovieRatingViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace MvcMovie.Models
+{
+    public class MovieRatingViewModel
+    {
+		public List<Movie> movies;
+		public SelectList ratings;
+		public string movieRating { get; set; }
+    }
+}
